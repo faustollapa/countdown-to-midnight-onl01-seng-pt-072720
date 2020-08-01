209ed6131f6148f282034f782daee9f1186e3e94
Fausto Llapa <faustollapa850@gmail.com>
2020-08-01 15:32:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1 
+  end  
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num_secs)
+  while num_secs >= 5
+    puts "Benchmark.measure"
+    number += 5
+  end  
+end  
