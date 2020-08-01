28263ff38715a8d97990614fbb20d8a285078ae2
Fausto Llapa <faustollapa850@gmail.com>
2020-08-01 15:40:46 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,7 +8,7 @@ def countdown(number)
   "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(num_secs)
+def countdown_with_sleep(number)
   while number > 0 
     puts "#{number} SECOND(S)!"
     sleep(1)
